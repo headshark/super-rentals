6f5f27b0af86f36713b04985a335382a5f3f7fff
Patrick <torralbapatrick15@gmail.com>
2018-12-22 17:14:41 -0600

    Add acceptance tests

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,26 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | list rentals', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('should show rentals as the home page', async function(assert) {
+  });
+
+  test('should link to information about the company', async function(assert) {
+  });
+
+  test('should link to contact information', async function(assert) {
+  });
+
+  test('should list available rentals', async function(assert) {
+  });
+
+  test('should filter the list of rentals by city', async function(assert) {
+  });
+
+  test('should show details for a selected rental', async function(assert) {
+  });
+
+});
