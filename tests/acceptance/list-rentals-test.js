660684c860199ab7385648e5bc4a34dbb1b72bfd
Patrick <torralbapatrick15@gmail.com>
2018-12-22 18:55:48 -0600

    Add tests for visiting rentals, about and contact page

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,17 +1,29 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import {
+  click,
+  currentURL,
+  visit
+} from '@ember/test-helpers'
 
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
 
   test('should show rentals as the home page', async function(assert) {
+    await visit('/');
+    assert.equal(currentURL(), '/rentals', 'should redirect automatically');
   });
 
   test('should link to information about the company', async function(assert) {
+    await visit('/');
+    await click(".menu-about");
+    assert.equal(currentURL(), '/about', 'should navigate to about');
   });
 
   test('should link to contact information', async function(assert) {
+    await visit('/');
+    await click(".menu-contact");
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
   });
 
   test('should list available rentals', async function(assert) {
