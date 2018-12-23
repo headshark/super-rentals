0b0ceda80227cab4ef9377d512d4274bb811f549
Patrick <torralbapatrick15@gmail.com>
2018-12-23 13:11:14 -0600

    Add tests for map services

--- tests/integration/components/location-map-test.js
+++ tests/integration/components/location-map-test.js
@@ -1,26 +1,31 @@
+import Service from '@ember/service';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { resolve } from 'rsvp';
 
-module('Integration | Component | location-map', function(hooks) {
+let StubMapsService = Service.extend({
+  getMapElement(location) {
+    this.set('calledWithLocation', location);
+    let element = document.createElement('div');
+    element.className = 'map';
+    return resolve(element);
+  }
+});
+
+module('Integration | Component | location map', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  hooks.beforeEach(function() {
+    this.owner.register('service:map-element', StubMapsService);
+    this.mapsService = this.owner.lookup('service:map-element');
+  });
 
-    await render(hbs`{{location-map}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#location-map}}
-        template block text
-      {{/location-map}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+  test('should append map element to container element', async function(assert) {
+    this.set('myLocation', 'New York');
+    await render(hbs`{{location-map location=myLocation}}`);
+    assert.ok(this.element.querySelector('.map-container > .map'), 'container should have map child');
+    assert.equal(this.get('mapsService.calledWithLocation'), 'New York', 'should call service with New York');
   });
 });
